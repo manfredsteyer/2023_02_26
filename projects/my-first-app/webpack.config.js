29bf89efb9ced56e7bd490cf7ca2c9522c7c5cad
ashwini-yenmakaje <ashwini.y@pacewisdom.com>
2023-01-25 22:36:42 +0530

    devServer header update

--- projects/my-first-app/webpack.config.js
+++ projects/my-first-app/webpack.config.js
@@ -26,7 +26,9 @@ module.exports = {
   },
   devServer: {
     headers: {
-      "Access-Control-Allow-Origin": "*"
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
     }
   },
   plugins: [
