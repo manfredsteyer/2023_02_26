f430da9dcee9d8bf5795b83fe191af2b92ad39c2
ashwini-yenmakaje <ashwini.y@pacewisdom.com>
2023-01-26 21:11:06 +0530

    header update

--- projects/shell/webpack.config.js
+++ projects/shell/webpack.config.js
@@ -25,6 +25,13 @@ module.exports = {
   experiments: {
     outputModule: true
   },
+  devServer: {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    }
+  },
   plugins: [
     new ModuleFederationPlugin({
         // library: { type: "module" },
